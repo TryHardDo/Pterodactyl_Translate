6c2afbfff664a511a3b979ec3edb49d2b28ae50e
Lévai Levente <51400985+TryHardDo@users.noreply.github.com>
2020-12-10 00:55:18 +0100

    auth.php fix
    Kis helyesírási és fogalmazási hiba javítása

--- hu/auth.php
+++ hu/auth.php
@@ -3,7 +3,7 @@
 return [
     'sign_in' => 'Belépés',
     'go_to_login' => 'Irány a Belépéshez',
-    'failed' => 'Nem található a megadott adatoknak mgefelelő fiók.',
+    'failed' => 'Nem található a megadott adatoknak megfelelő fiók.',
 
     'forgot_password' => [
         'label' => 'Elfelejtetted a jelszavad?',
@@ -16,12 +16,12 @@ return [
     ],
 
     'two_factor' => [
-        'label' => '2-Faktor kulcs',
+        'label' => '2-Faktoros kulcs',
         'label_help' => 'További azonosításra van szükség a folytatás érdekében. Kérlek írd be a készüléked által generált kódot hogy véglegesítsd a belépést.',
         'checkpoint_failed' => 'A 2-Faktoros megerősítő kód érvénytelen.',
     ],
 
-    'throttle' => 'Túl sok belépési kísérle. Kérlek próbáld meg :seconds másodperc múlva.',
-    'password_requirements' => 'A jelszónak legalább 8 karakter hosszúnak kell lennie és egyedinek.',
+    'throttle' => 'Túl sok belépési kísérlet. Kérlek próbáld meg :seconds másodperc múlva.',
+    'password_requirements' => 'A jelszónak legalább 8 karakter hosszúnak és egyedinek kell lennie.',
     '2fa_must_be_enabled' => 'A rendszergazda előírta, hogy a panel használatához engedélyezni kell a 2-Faktoros hitelesítést a fiókjában.',
 ];
