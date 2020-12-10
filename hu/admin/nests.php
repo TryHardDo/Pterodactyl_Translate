adf155e55d647e20b17dd36cea77b5f349049928
TryHardDo <51400985+TryHardDo@users.noreply.github.com>
2020-12-10 08:26:59 +0100

    Translated nests.php

--- hu/admin/nests.php
+++ hu/admin/nests.php
@@ -9,25 +9,25 @@
 
 return [
     'notices' => [
-        'created' => 'A new nest, :name, has been successfully created.',
-        'deleted' => 'Successfully deleted the requested nest from the Panel.',
-        'updated' => 'Successfully updated the nest configuration options.',
+        'created' => ':name nevű Nest sikeresen létrehozva.',
+        'deleted' => 'A kért Nest sikeresen törölve a panelből.',
+        'updated' => 'A Nest konfigurációjának beállításai sikeresen frissítve.',
     ],
     'eggs' => [
         'notices' => [
-            'imported' => 'Successfully imported this Egg and its associated variables.',
-            'updated_via_import' => 'This Egg has been updated using the file provided.',
-            'deleted' => 'Successfully deleted the requested egg from the Panel.',
-            'updated' => 'Egg configuration has been updated successfully.',
-            'script_updated' => 'Egg install script has been updated and will run whenever servers are installed.',
-            'egg_created' => 'A new egg was laid successfully. You will need to restart any running daemons to apply this new egg.',
+            'imported' => 'Sikeresen importálta ezt a tojást és a hozzá tartozó változókat.',
+            'updated_via_import' => 'Ezt a Tojást a mellékelt file által frissítettük.',
+            'deleted' => 'Sikeresen törölve a kért Egg a panelből.',
+            'updated' => 'Az Egg configurációja sikeresen frissítve.',
+            'script_updated' => 'Az Egg telepítő szkript frissítve és a szerver telepítésekor lefut.',
+            'egg_created' => 'Az új Egg sikeresen letrehozva. Az új Egg alkalmazásához újra kell indítania a futó daemonokat.',
         ],
     ],
     'variables' => [
         'notices' => [
-            'variable_deleted' => 'The variable ":variable" has been deleted and will no longer be available to servers once rebuilt.',
-            'variable_updated' => 'The variable ":variable" has been updated. You will need to rebuild any servers using this variable in order to apply changes.',
-            'variable_created' => 'New variable has successfully been created and assigned to this egg.',
+            'variable_deleted' => 'A változó ":variable" törölve lett, és az újjáépítés után már nem lesz elérhető a szerverek számára.',
+            'variable_updated' => 'A változó ":variable" frissítve lett. A változások alkalmazásához minden szervert újra kell építeni ezzel a változóval.',
+            'variable_created' => 'Új változó sikeresen létrehozva és hozzárendelve ehhez az Egg-hez.',
         ],
     ],
 ];
