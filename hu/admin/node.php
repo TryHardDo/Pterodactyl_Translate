35e217c116d1a6c01e0fb05c8767c9fccd9d88f2
TryHardDo <51400985+TryHardDo@users.noreply.github.com>
2020-12-13 20:45:25 +0100

    Translated node.php
    Lefordításra került a node.php
    Kérlek titeket ha valami hibát észrevesztek vagy a megfogalmazásra jobb öteltetek van jelezzétek!

--- hu/admin/node.php
+++ hu/admin/node.php
@@ -9,15 +9,15 @@
 
 return [
     'validation' => [
-        'fqdn_not_resolvable' => 'The FQDN or IP address provided does not resolve to a valid IP address.',
-        'fqdn_required_for_ssl' => 'A fully qualified domain name that resolves to a public IP address is required in order to use SSL for this node.',
+        'fqdn_not_resolvable' => 'A megadott FQDN vagy IP-cím nem oldódik fel érvényes IP-címre.',
+        'fqdn_required_for_ssl' => 'Az SSL használatához ennél a csomópontnál egy teljesen minősített, nyilvános IP-címre megoldódó domain névre (FQDN) van szükség.',
     ],
     'notices' => [
-        'allocations_added' => 'Allocations have successfully been added to this node.',
-        'node_deleted' => 'Node has been successfully removed from the panel.',
-        'location_required' => 'You must have at least one location configured before you can add a node to this panel.',
-        'node_created' => 'Successfully created new node. You can automatically configure the daemon on this machine by visiting the \'Configuration\' tab. <strong>Before you can add any servers you must first allocate at least one IP address and port.</strong>',
-        'node_updated' => 'Node information has been updated. If any daemon settings were changed you will need to reboot it for those changes to take effect.',
-        'unallocated_deleted' => 'Deleted all un-allocated ports for <code>:ip</code>.',
+        'allocations_added' => 'A kiosztások sikeresen hozzáadva ehhez a csomóponthoz.',
+        'node_deleted' => 'Csomópont sikeresen törölve a panelből.',
+        'location_required' => 'Lennie kell minimum egy, már konfigurált elhelyezkedésnek mielőtt hozzáadnál egy csomópontot a panelhez.',
+        'node_created' => 'Sikeresen létrehozva egy új csomópont. Automatikusan konfigurálhatod ezt a daemon-t ha ellátogatsz a \'Konfiguráció\' fülre. <strong>A szerverek hozzáadása előtt ki kell osztanod legalább egy IP-címet és portot.</strong>',
+        'node_updated' => 'A csomópont információi frissültek. Ha a daemon beállításait megváltoztattad, újra kell indítanod, hogy ezek a változtatások életbe lépjenek.',
+        'unallocated_deleted' => '<code>:ip</code> összes kiosztatlan portja törölve lett.',
     ],
 ];
